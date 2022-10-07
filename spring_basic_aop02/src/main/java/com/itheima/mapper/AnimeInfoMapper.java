1f224a25fec469a6626ad0ce6f34964d923e7b72
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-07 11:09:57 +0800

    AM 01st commit

---
+++ spring_basic_aop02/src/main/java/com/itheima/mapper/AnimeInfoMapper.java
@@ -0,0 +1,20 @@
+package com.itheima.mapper;
+
+import com.itheima.pojo.AnimeInfo;
+
+import java.util.List;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> spring_basic_aop
+ * BelongsPackage ==> com.itheima.mapper
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-07 11:05:12
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+public interface AnimeInfoMapper {
+
+    List<AnimeInfo> selectAll();
+
+
+}
---
+++ spring_basic_aop02/src/main/java/com/itheima/service/AnimeInfoService.java
@@ -0,0 +1,21 @@
+package com.itheima.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> spring_basic_aop
+ * BelongsPackage ==> com.itheima.service
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-07 11:09:37
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+public class AnimeInfoService {
+
+
+
+
+
+}
+
+
